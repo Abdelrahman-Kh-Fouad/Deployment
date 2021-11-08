e49579300c925528ce4c85d91bb6d3b4c535135b
Abdelrahman Khaled <abdokh950@gmail.com>
2021-11-08 19:13:11 +0200

    Copy FApp model because i didn't find our demo model

---
+++ app/app.py
@@ -0,0 +1,51 @@
+import os
+import uuid
+
+from flask import Flask, request, json, jsonify
+from keras.models import load_model
+
+import model
+
+dl_model = load_model('./mymodel.h5')
+app = Flask(__name__)
+
+app.secret_key = "cov-19"
+UPLOAD_FOLDER = './static'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+
+@app.route('/')
+def hello_world():  # put application's code here
+    return jsonify(
+        info='Upload files to /upload path Using POST Request , File key is (img)',
+    )
+
+
+@app.route('/upload', methods=['GET', 'POST'])
+def upload():
+    if request.method == 'POST':
+        file = request.files['img']
+        extension = os.path.splitext(file.filename)[1]
+        f_name = str(uuid.uuid4()) + extension
+        path = os.path.join(app.config['UPLOAD_FOLDER'], f_name)
+        file.save(path)
+        result = model.runModel(path, dl_model)
+        os.remove(path)
+        return jsonify(
+            status='success',
+            data=result,
+            msg='',
+            code=200
+        )
+        # return render_ #str(model.runModel(path))
+    else:
+        return jsonify(
+            status='fail',
+            data=None,
+            msg='Method Not Allowed',
+            code=405
+        )
+
+
+if __name__ == '__main__':
+    app.run()
