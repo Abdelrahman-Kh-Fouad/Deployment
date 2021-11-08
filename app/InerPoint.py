b8b03476d29f29cc46af580cb8659210036f2993
Abdelrahman Khaled <abdokh950@gmail.com>
2021-11-08 21:57:04 +0200

    Add gunicorn script

--- app/InerPoint.py
+++ app/InerPoint.py
@@ -1,10 +1,16 @@
 import re
 import subprocess
 import sys
-import os 
-url = "https://drive.google.com/file/d/1Me7qeEZ5K2ECMPTiqICDQurx_wHARXgm/view"
+import os
+if __name__ == '__main__':
 
-fileName = re.findall( r'https://drive.google.com/file/d/(.*)/view' ,url)[0]    
-download = subprocess.run(f'{sys.path[0]}/InerPointScripts/wget_gdrive.sh {fileName} mymodel.h5' ,shell=True)
-# while download.stdout : 
-#     print(download.stdout)
+    url = "https://drive.google.com/file/d/1Me7qeEZ5K2ECMPTiqICDQurx_wHARXgm/view"
+    modeName = 'mymodel.h5'
+    currentPath = sys.path[0]
+    print(os.listdir(currentPath))
+    if not (modeName in os.listdir(currentPath)):
+        fileName = re.findall( r'https://drive.google.com/file/d/(.*)/view' ,url)[0]
+        download = subprocess.run(f'{sys.path[0]}/InerPointScripts/wget_gdrive.sh {fileName}  {modeName}',shell=True)
+        # while download.stdout :
+        #     print(download.stdout)
+    subprocess.run('gunicorn -b 0.0.0.0:8000 app:app',shell=True)
